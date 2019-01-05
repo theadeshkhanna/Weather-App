91db880079bd04fcbde8840ef47c35f8d942ec57
Adesh Khanna <adeshkhanna271@gmail.com>
2019-01-05 14:27:25 +0530

    second

--- playground/app.js
+++ playground/app.js
@@ -1,8 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+    .option({
+        a: {
+            demand: true,
+            alias: 'Address',
+            describe: 'Name of city whose weather is to be known',
+            string: true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+    var city = argv.address;
+    var apiKey = 'a008aa6283708efd9eaea105aea71740';
 request({
-    url: 'https://samples.openweathermap.org/data/2.5/weather?q=Londo, uk&appid=b6907d289e10d714a6e88b30761fae22',
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`,
     json:true
 },(error,response,body)=> {
-    console.log(`Longitude : ${body.coord.lon} Latitude : ${body.coord.lat}`);
+     if(error){
+        console.log('Unable to connect to the servers');
+    }else{
+        console.log(`Temperature of ${body.name} = ${body.main.temp}°C`);
+    }
 }); 
