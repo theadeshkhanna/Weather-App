0fa66f8bd4e3b2ed302a4cb3aec23363fb39c8d1
Adesh Khanna <adeshkhanna271@gmail.com>
2019-01-19 16:43:44 +0530

    handlebars added

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const geocode = require('./geocode/geocode');
+
+const port = 4000;
+
+
+var app = express();  //express 
+
+app.engine('handlebars', exphbs({   //handlebars middleware
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));  //bodyparser middleware body parser
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {    //loading home page
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {          //loading about page
+    res.render('about');
+});
+
+app.post('/index', (req, res) => {
+    var City = JSON.stringify(req.body.cityName, undefined, 2);
+
+    geocode.geocodeAddress(City, (errorMsg, results) => {
+        if (errorMsg) {
+            console.log(errorMsg);
+        } else {
+            res.render('/', {
+                Temperature: results.Temperature,
+                city: results.city
+            })
+        }
+    });
+    
+});
+
+app.listen(port, () => {
+    console.log(`server is up and running at port ${port}`);
+})
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
+
 var geocodeAddress = (address, callback) => {
     var apiKey = 'a008aa6283708efd9eaea105aea71740';
+    var name = JSON.parse(address);
     request({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${apiKey}`,
-        json:true
-    },(error,response,body)=> {
-         if(error){
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${apiKey}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
             callback('Unable to connect to the servers');
-        }else{
-            callback(undefined,{
+        } else {
+            callback(undefined, {
                 Temperature: body.main.temp,
                 city: body.name
-            });           
+            });
         }
-    }); 
-    
+    });
+
 };
 
 module.exports.geocodeAddress = geocodeAddress;
